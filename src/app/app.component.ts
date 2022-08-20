002d1c4eafda8494b8d081b3f433225755500294
edjunior <lucatinho_1@hotmail.com>
2022-08-20 15:25:48 -0300

    cardapio online

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cardapio';
+  title = 'Cardápio';
+
+  products = [
+    {
+      nome: 'Pão na Chapa',
+      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ingredientes: 'Hambúrguer artesanal de 130 gramas, 2 ovos separados por finas fatias de linguiça calabresa defumada, envolvido por queijo muçarela derretido na chapa, alface e tomate.',
+      preco: '22.20'
+    }, {
+      nome: 'Pão na Chapa',
+      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ingredientes: 'Hambúrguer artesanal de 130 gramas, 2 ovos separados por finas fatias de linguiça calabresa defumada, envolvido por queijo muçarela derretido na chapa, alface e tomate.',
+      preco: '22.00'
+    }, {
+      nome: 'Pão na Chapa',
+      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ingredientes: 'Hambúrguer artesanal de 130 gramas, 2 ovos separados por finas fatias de linguiça calabresa defumada, envolvido por queijo muçarela derretido na chapa, alface e tomate.',
+      preco: '22.14'
+    }, {
+      nome: 'Pão na Chapa',
+      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ingredientes: 'Hambúrguer artesanal de 130 gramas, 2 ovos separados por finas fatias de linguiça calabresa defumada, envolvido por queijo muçarela derretido na chapa, alface e tomate.',
+      preco: '22.17'
+    }, {
+      nome: 'Pão na Chapa',
+      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ingredientes: 'Hambúrguer artesanal de 130 gramas, 2 ovos separados por finas fatias de linguiça calabresa defumada, envolvido por queijo muçarela derretido na chapa, alface e tomate.',
+      preco: '22.80'
+    }, {
+      nome: 'Pão na Chapa',
+      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ingredientes: 'Hambúrguer artesanal de 130 gramas, 2 ovos separados por finas fatias de linguiça calabresa defumada, envolvido por queijo muçarela derretido na chapa, alface e tomate.',
+      preco: '22.99'
+    },
+  ]
 }
