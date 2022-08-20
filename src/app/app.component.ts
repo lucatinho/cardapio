a0f7de90f5fb6860fe093d7cf3ad9234b72fd83d
edjunior <lucatinho_1@hotmail.com>
2022-08-20 15:37:16 -0300

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,33 +10,33 @@ export class AppComponent {
 
   products = [
     {
-      nome: 'Pão na Chapa',
-      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      nome: 'X - Monstro',
+      img: 'https://cdn.pixabay.com/photo/2019/01/29/18/05/burger-3962996_960_720.jpg',
       ingredientes: 'Hambúrguer artesanal de 130 gramas, 2 ovos separados por finas fatias de linguiça calabresa defumada, envolvido por queijo muçarela derretido na chapa, alface e tomate.',
       preco: '22.20'
     }, {
-      nome: 'Pão na Chapa',
-      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      nome: 'X - Monstro',
+      img: 'https://cdn.pixabay.com/photo/2019/01/29/18/05/burger-3962996_960_720.jpg',
       ingredientes: 'Hambúrguer artesanal de 130 gramas, 2 ovos separados por finas fatias de linguiça calabresa defumada, envolvido por queijo muçarela derretido na chapa, alface e tomate.',
       preco: '22.00'
     }, {
-      nome: 'Pão na Chapa',
-      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      nome: 'X - Monstro',
+      img: 'https://cdn.pixabay.com/photo/2019/01/29/18/05/burger-3962996_960_720.jpg',
       ingredientes: 'Hambúrguer artesanal de 130 gramas, 2 ovos separados por finas fatias de linguiça calabresa defumada, envolvido por queijo muçarela derretido na chapa, alface e tomate.',
       preco: '22.14'
     }, {
-      nome: 'Pão na Chapa',
-      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      nome: 'X - Monstro',
+      img: 'https://cdn.pixabay.com/photo/2019/01/29/18/05/burger-3962996_960_720.jpg',
       ingredientes: 'Hambúrguer artesanal de 130 gramas, 2 ovos separados por finas fatias de linguiça calabresa defumada, envolvido por queijo muçarela derretido na chapa, alface e tomate.',
       preco: '22.17'
     }, {
-      nome: 'Pão na Chapa',
-      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      nome: 'X - Monstro',
+      img: 'https://cdn.pixabay.com/photo/2019/01/29/18/05/burger-3962996_960_720.jpg',
       ingredientes: 'Hambúrguer artesanal de 130 gramas, 2 ovos separados por finas fatias de linguiça calabresa defumada, envolvido por queijo muçarela derretido na chapa, alface e tomate.',
       preco: '22.80'
     }, {
-      nome: 'Pão na Chapa',
-      img: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      nome: 'X - Monstro',
+      img: 'https://cdn.pixabay.com/photo/2019/01/29/18/05/burger-3962996_960_720.jpg',
       ingredientes: 'Hambúrguer artesanal de 130 gramas, 2 ovos separados por finas fatias de linguiça calabresa defumada, envolvido por queijo muçarela derretido na chapa, alface e tomate.',
       preco: '22.99'
     },
